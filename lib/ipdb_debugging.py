71d7e9b83ca763d1a55411188fb0a46beaa6fe42
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-11-22 13:44:00 +0300

    testing in python

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,6 @@
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num += 2  # This line adds 2 to num and assigns the result back to num
+    ipdb.set_trace()  # This sets a breakpoint for debugging
     return num
